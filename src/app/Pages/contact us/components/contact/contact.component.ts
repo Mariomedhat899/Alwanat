73576d2f8ca146bd5abb94f1865fb50b77712b25
Mariomedhat899 <mariomedhat899@gmail.com>
2025-08-13 20:10:01 +0300

    "third commit"

--- src/app/Pages/contact us/components/contact/contact.component.ts
+++ src/app/Pages/contact us/components/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { TranslatePipe } from '@ngx-translate/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact',
@@ -8,5 +8,34 @@ import { TranslatePipe } from '@ngx-translate/core';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+ 
+ 
+  constructor(private translate: TranslateService) {}
+  toEmail = 'alwanatgroup@gmail.com';
+ facebookHref ='https://www.facebook.com/AlwanatAcademy'
+  whatsappNumber = '+201033100598';
+  whatsappMessage = 'Hello, I would like to know more about your services.';
 
+get whatsappHref(): string {
+  const number = this.whatsappNumber.replace(/\+/g, ''); // remove +
+  return `https://wa.me/${number}?text=${encodeURIComponent(this.whatsappMessage)}`;
+}
+
+
+
+ get mailtoHref(): string {
+    const subject = this.translate.instant('contact.mailtoSubject');
+    const body = this.translate.instant('contact.mailtoBody');
+    return `mailto:${this.toEmail}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+  }
+  get gmailHref(): string {
+    const subject = this.translate.instant('contact.mailtoSubject');
+    const body = this.translate.instant('contact.mailtoBody');
+    // Gmail compose fallback
+    return `https://mail.google.com/mail/?view=cm&to=${encodeURIComponent(this.toEmail)}&su=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+  }
+
+  openMailto(): void {
+    window.location.href = this.mailtoHref;
+  }
 }
